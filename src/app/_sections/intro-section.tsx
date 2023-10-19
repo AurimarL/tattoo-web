fab386744df29004d9d903a66346401425aa2c22
Aurimar <aurimardev@gmail.com>
2023-10-19 20:08:54 +0100

    bg

--- src/app/_sections/intro-section.tsx
+++ src/app/_sections/intro-section.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 export default function IntroSection() {
   return (
     <section className=" w-full">
-      <div className="h-96 w-full md:h-[500px] bg-black">
-        <div className="flex justify-center items-center h-full">
-          <h1 className="text-red-500 ">Big image with Text here</h1>
+      <div className="h-96 w-full md:h-[500px] bg-black relative">
+        <Image priority alt="" src="/bg.png" fill />
+        <div className="flex justify-center relative items-center h-full">
+          <h1 className="text-white text-3xl ">Big image with Text here</h1>
         </div>
       </div>
     </section>
